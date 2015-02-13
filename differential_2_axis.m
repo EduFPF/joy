d683f2bd7e2204a6123f521e6ddd85e5146556df
Eduardo <punk.lagwagon@gmail.com>
2015-02-13 18:10:40 -0300

    dash name e atualizacao das operacoes matematicas

--- differential_2_axis.m
+++ differential_2_axis.m
@@ -2,7 +2,7 @@
 % Eduardo Figueiredo Porto Filho
 % Engenharia Elétrica - Universidade Federal de Campina Grande
 
-joy = vrjoystick(1);
+%joy = vrjoystick(1);
 MOV = [0 0 0];
 eixoX = [1 0];
 eixoY = [0 1];
@@ -20,10 +20,19 @@ while true
     cosAnguloY = dot(dir,eixoY)/(norm(dir)*norm(eixoY));
     anguloY = acos(cosAnguloY)*180/pi; %Graus
     
-    vel = norm(dir); %(x^2 + y^2)^(1/2)
+    vel = norm(dir)*100; %(x^2 + y^2)^(1/2)
     
-    for i = 0; i <= 2; i++
-        MOV(1,i) = rem(vel,10);
+    if vel > 100
+        vel = 100;
+    end
+    
+    %for i = 0; i <= 2; i++
+     %MOV(1,i) = rem(vel,10);
+        %vel = vel / 10;
+    %end
+    
+    for i = 1:3
+        MOV(1,i) = int64(rem(vel,10));
         vel = vel / 10;
     end
     
@@ -35,9 +44,11 @@ while true
         AX = 2;
     elseif x < 0 && anguloY > 45 && anguloY < 135
         AX = -2;
+    else
+        AX = 1;
     end
     
-    for i = 0; i <= 2; i++
+    for i = 1:3
         %fwrite (joy, MOV(1,i);
         MOV(1,i)
     end
